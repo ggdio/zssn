eea11e7ee1c3b0658415442444a294529d250ac2
Guilherme Dio <ggrdio@gmail.com>
2016-06-07 02:24:12 -0300

    Domain model

---
+++ src/main/java/br/com/ggdio/zssn/domain/enumeration/ItemType.java
@@ -0,0 +1,20 @@
+package br.com.ggdio.zssn.domain.enumeration;
+
+public enum ItemType {
+
+	WATER(4),
+	FOOD(3),
+	MEDICATION(2),
+	AMMUNITION(1)
+	;
+	
+	private final int points;
+	ItemType(int points) {
+		this.points = points;
+	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+}
--- src/main/java/br/com/ggdio/zssn/domain/model/Person.java
+++ src/main/java/br/com/ggdio/zssn/domain/model/Person.java
@@ -1,8 +1,14 @@
 package br.com.ggdio.zssn.domain.model;
 
 import java.time.LocalDate;
+import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 import br.com.ggdio.zssn.domain.enumeration.Gender;
 import br.com.ggdio.zssn.domain.enumeration.Status;
@@ -10,14 +16,85 @@ import br.com.ggdio.zssn.domain.enumeration.Status;
 @Entity
 public class Person {
 	
+	@Id @GeneratedValue
+	private Long id;
+	
+	@Column(nullable=false)
 	private String name;
+	
+	@Column(nullable=false)
 	private LocalDate birthday;
+	
+	@Column(nullable=false)
 	private Gender gender;
+	
+	@Column(nullable=false)
 	private Status status;
+	
+	@Column(nullable=false)
 	private Location location;
 	
+	@OneToMany @JoinColumn(name="person_id")
+	private Set<Item> items;
+	
 	public Person() {
-		this.status = Status.ALIVE;
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public Set<Item> getItems() {
+		return items;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public void setItems(Set<Item> items) {
+		this.items = items;
 	}
 
 }
